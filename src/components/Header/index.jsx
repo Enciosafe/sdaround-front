4daeb9a7e69cfda5a5ba864b31fa9785532f64b5
Stas <finneganlucky@gmail.com>
2022-12-30 12:47:09 +0100

    routers

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import { Link } from 'react-router-dom'
 
 import styles from "./Header.module.scss";
 import Container from "@mui/material/Container";
 
 export const Header = () => {
+    const isAuth = false
+
+    const onClickLogout = () => {}
+
     return (
         <div className={styles.root}>
             <Container maxWidth="lg">
                 <div className={styles.inner}>
-                    <a className={styles.logo} href="/">
+                    <Link className={styles.logo} to="/">
                         <div>SD_Around</div>
-                    </a>
+                    </Link>
                     <div className={styles.buttons}>
-                        <Button variant="outlined">Зайти</Button>
-                        <Button variant="contained">Створити аккаунт</Button>
+                        {isAuth ? (
+                            <>
+                                <Link to="/posts/create" style={{ textDecoration: 'none' }} >
+                                    <Button variant="contained">Написати статтю</Button>
+                                </Link>
+                                <Button onClick={onClickLogout} variant='contained' color='error' >
+                                    Вийти
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <Link to="/login" style={{ textDecoration: 'none' }}>
+                                    <Button variant="outlined">Зайти</Button>
+                                </Link>
+                                <Link to="/register" style={{ textDecoration: 'none' }}>
+                                    <Button variant="contained">Створити аккаунт</Button>
+                                </Link>
+                            </>
+                        )}
                     </div>
                 </div>
             </Container>
--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import clsx from "clsx";
 import IconButton from "@mui/material/IconButton";
+import { Link } from 'react-router-dom'
 import DeleteIcon from "@mui/icons-material/Clear";
 import EditIcon from "@mui/icons-material/Edit";
 import EyeIcon from "@mui/icons-material/RemoveRedEyeOutlined";
@@ -11,7 +12,7 @@ import { UserInfo } from "../UserInfo";
 import { PostSkeleton } from "./Skeleton";
 
 export const Post = ({
-                         id,
+                         _id,
                          title,
                          createdAt,
                          imageUrl,
@@ -28,35 +29,42 @@ export const Post = ({
         return <PostSkeleton />;
     }
 
+    const onClickRemove = () => {}
+
     return (
         <div className={clsx(styles.root, { [styles.rootFull]: isFullPost })}>
             {isEditable && (
                 <div className={styles.editButtons}>
-                    <IconButton color="primary">
-                        <EditIcon />
-                    </IconButton>
-                    <IconButton color="secondary">
+                    <Link to={`/posts/${_id}/edit`}>
+                        <IconButton color="primary">
+                            <EditIcon />
+                        </IconButton>
+                    </Link>
+                    <IconButton onClick={onClickRemove} color="secondary">
                         <DeleteIcon />
                     </IconButton>
                 </div>
             )}
-            <img
-                className={clsx(styles.image, { [styles.imageFull]: isFullPost })}
-                src={imageUrl}
-                alt={title}
-            />
+
+            {imageUrl && (
+                <img
+                    className={clsx(styles.image, { [styles.imageFull]: isFullPost })}
+                    src={imageUrl}
+                    alt={title}
+                />
+            )}
             <div className={styles.wrapper}>
                 <UserInfo {...user} additionalText={createdAt} />
                 <div className={styles.indention}>
                     <h2
                         className={clsx(styles.title, { [styles.titleFull]: isFullPost })}
                     >
-                        {isFullPost ? title : <a href={`/posts/${id}`}>{title}</a>}
+                        {isFullPost ? title : <Link to={`/posts/${_id}`}>{title}</Link>}
                     </h2>
                     <ul className={styles.tags}>
                         {tags.map((name) => (
                             <li key={name}>
-                                <a href={`/tag/${name}`}>#{name}</a>
+                                <Link to={`/tag/${name}`}>#{name}</Link>
                             </li>
                         ))}
                     </ul>
